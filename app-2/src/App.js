ff432687a9ba835da3b7295404c191069d63cd4f
Richard Miller <richardmmill@gmail.com>
2021-03-03 17:02:04 -0500

    Richards work on Lab 3.3

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      listOfItems: ["spaghetti", "ice cream", "sushi", "bologna", "cheese"]
+    }
+  }
+
+
+
+  render() {
+    let displayList = this.state.listOfItems.map((elem, index) => {
+      return <h2 key={index}>{elem}</h2>
+    });
+    return
+    <div className='App'>{displayList}</div>
+  }
 }
 
 export default App;

c2d70a1afb5c739c655637d2cfaaca4089b09b85
Richard Miller <richardmmill@gmail.com>
2021-03-04 12:58:09 -0500

    Richards work on Lab 3.3

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      character: [],
+      homeworld: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://swapi.dev/api/people/4/').then(response => {
+      this.setState({ charater: response.data })
+    })
+  }
+  componentDidMount() {
+    axios.get('http://swapi.dev/api/planets/1/').then(response => {
+      this.setState({ homeowold: response.data })
+    })
+
+  }
+
+
+  render() {
+    return (
+      <div className="App" >
+        <h1>Name: {this.state.character.name}</h1>
+        <h1>Height: {this.state.character.height}</h1>
+        <h1>Birth Year: {this.state.character.birth_year}</h1>
+        <h1>Eye Color: {this.state.character.eye_color}</h1>
+        <h1>Skin Color {this.state.character.skin_color}</h1>
+        <h1>Homeworld: {this.state.homeworld.name}</h1>
+      </div>
+    );
+  }
 }
+
 
 export default App;

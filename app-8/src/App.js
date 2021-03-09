6ddecca3366b782b4b98296bc2ca52e68076a34f
Richard Miller <richardmmill@gmail.com>
2021-03-08 20:54:59 -0500

    Richards work on Lab 3.3

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -13,27 +13,27 @@ class App extends Component {
   }
 
   componentDidMount() {
-    axios.get('https://swapi.dev/api/people/4/').then(response => {
-      this.setState({ charater: response.data })
-    })
+    axios.get('https://swapi.dev/api/people/4/')
+      .then(response => {
+        console.log(response)
+        this.setState({ character: response.data })
+      })
+      .catch(error => console.log(error))
   }
-  componentDidMount() {
-    axios.get('http://swapi.dev/api/planets/1/').then(response => {
-      this.setState({ homeowold: response.data })
-    })
 
-  }
 
 
   render() {
+
+    console.log(this.state.character)
     return (
       <div className="App" >
         <h1>Name: {this.state.character.name}</h1>
         <h1>Height: {this.state.character.height}</h1>
         <h1>Birth Year: {this.state.character.birth_year}</h1>
         <h1>Eye Color: {this.state.character.eye_color}</h1>
-        <h1>Skin Color {this.state.character.skin_color}</h1>
-        <h1>Homeworld: {this.state.homeworld.name}</h1>
+        <h1>Skin Color: {this.state.character.skin_color}</h1>
+        {/* <h1>Homeworld: {this.state.character.homeworld}</h1> */}
       </div>
     );
   }
